6298c71d8f20144fd43a1847fbcff49befc91143
DESKTOP-ACGD89T\SonyTuladhar <sony@aerion.com.au>
2022-09-13 14:10:03 +0545

    Object class created and xpath added

--- src/Login/LoginObject.java
+++ src/Login/LoginObject.java
@@ -1,5 +1,39 @@
 package Login;
 
-public class LoginObject {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LoginObject {
+	WebDriver driver;
+	
+	public LoginObject(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//input[@name='username']")
+	WebElement username;
+
+
+	public void usernameInput(String user) {
+		username.sendKeys(user);
+	}
+
+	@FindBy(xpath = "//input[@name='password']")
+	WebElement password;
+
+
+	public void passwordInput(String pass) {
+		password.sendKeys(pass);
+	}
+
+	@FindBy(xpath = "//button[@type='submit']")
+	WebElement login;
+
+	public void logInClick() {
+		login.click();
+	}
 }
+
