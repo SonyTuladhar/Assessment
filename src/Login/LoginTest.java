2092bde5b24c9b0d1435bf02e4a8fadae7d95855
DESKTOP-ACGD89T\SonyTuladhar <sony@aerion.com.au>
2022-09-13 14:25:15 +0545

    Web driver updated, created test data, created test class

---
+++ src/Login/LoginTest.java
@@ -0,0 +1,40 @@
+package Login;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void testLogin() {
+		System.setProperty("webdriver.chrome.driver", "webDriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+	}
+
+	@Test(dataProvider = "LoginDatas", dataProviderClass = LoginData.class, priority = 1)
+	public void loginData(String Username, String Password) throws InterruptedException {
+		LoginObject login = new LoginObject(driver);
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+		login.usernameInput(Username);
+		login.passwordInput(Password);
+		login.logInClick();
+		
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("scroll(0, 500);");
+		Thread.sleep(1000);
+	}
+	
+	@AfterMethod
+	public void driverClose() {
+		driver.close();
+	}
+}
